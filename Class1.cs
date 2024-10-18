c0a949bd061b5e6b41016a60daa925efdea44ede
Bror Lundberg <maximillian.lundber@chasacademy.se>
2024-10-18 16:25:34 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5_OOP
+{
+    internal class Class1
+    {
+    }
+    public class Circel
+    {
+        public Circel(int radie)
+        {
+            this.radie = radie;
+        }
+        public int radie { get; set; }
+        public double Get_Area()
+        {
+            return radie * radie * Math.PI;
+
+        }
+        public double Omkrets()
+        {
+            return 2 * Math.PI * radie;
+        }
+        public double Volym()
+        {
+            return (4 * Math.PI * Math.Pow(radie, 3)) / 3;
+        }
+    }
+    public class Triangel
+    {
+        public Triangel(int hypotenusa, int kateA, int kateB)
+        {
+            this.hypotenusa = hypotenusa;
+            this.katetA = kateA;
+            this.katetB = kateB;
+        }
+
+        public int hypotenusa { get; set; }
+        public double katetA { get; set; }
+        public double katetB { get; set; }
+
+        public double Area()
+        {
+            return (katetA * katetB) / 2;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circel shape = new Circel(5);
+
+            Circel shape2 = new Circel(6);
+            Triangel shape3 = new Triangel(4, 5, 3);
+            Console.WriteLine(shape.Get_Area());
+            Console.WriteLine(shape2.Get_Area());
+            Console.WriteLine(shape.Omkrets());
+            Console.WriteLine(shape2.Omkrets());
+            Console.WriteLine(shape.Volym());
+            Console.WriteLine(shape3.Area());
+
+        }
+    }
+}
